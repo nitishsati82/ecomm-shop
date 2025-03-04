769d603c8b3be0a8db5b1f14973e119551868cbf
NITISH SATI <nitishsati8@gmail.com>
2025-03-04 15:30:55 +0530

    url changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -87,7 +87,7 @@ function Header() {
   const signOutRedirect = () => {
     auth.removeUser();
     const clientId = "7gp6ln0g9gek7h4nkjru0d98tm";
-    const logoutUri = "http://localhost:3000/";
+    const logoutUri = "https://ecomtry.infy.uk/";
     const cognitoDomain = "https://ap-south-1kbml7t4jr.auth.ap-south-1.amazoncognito.com";
     window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
   };
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const cognitoAuthConfig = {
   authority: "https://cognito-idp.ap-south-1.amazonaws.com/ap-south-1_kbml7T4Jr",
   client_id: "7gp6ln0g9gek7h4nkjru0d98tm",
-  redirect_uri: "http://localhost:3000/",
+  redirect_uri: "https://ecomtry.infy.uk/",
   response_type: "code",
   scope: "aws.cognito.signin.user.admin email openid phone profile",
 };
